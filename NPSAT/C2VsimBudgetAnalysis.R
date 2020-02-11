7379146a4c49fbfa2f46ccceb01ad53b678e9068
George Kourakos <giorgk@gmail.com>
2020-02-11 05:28:43 +0200

    Update budget analysis

--- NPSAT/C2VsimBudgetAnalysis.R
+++ NPSAT/C2VsimBudgetAnalysis.R
@@ -1,163 +1,56 @@
 library(hdf5r)
 library(pracma)
 library(Hmisc)
+library(rgdal)
+library(plotly)
 
-c2vsim_path <- "../../C2Vsim_FG_v2/C2VSimFG-BETA_PublicRelease/"
+c2vsim_path <- "../c2vsimfg_beta2_publicrelease/C2VSimFG_BETA2_PublicRelease/"
 results_path <- paste0(c2vsim_path, "Results/")
 c2vsim_tm <- seq.Date(from = as.Date("1973/10/1"),to = as.Date("2015/09/1"),by = "month")
 ndays <- monthDays(c2vsim_tm)
 
-GW_BDGinfo <- H5File$new(filename = paste0(results_path, "C2VSimFG_GW_ZBudget.hdf"),mode = "r+")
-info <- GW_BDGinfo$ls(recursive = T)
-Attribute_names <- GW_BDGinfo[[info$name[6]]][]
+GW_BUD <- H5File$new(filename = paste0(results_path, "C2VSimFG_GW_ZBudget.hdf"),mode = "r+")
+info <- GW_BUD$ls(recursive = T)
+Attribute_names <- GW_BUD[[info$name[6]]][]
+Attribute_names <- trimws(Attribute_names,which = c("both", "left", "right"), whitespace = "[ \t\r\n]")
+info_names <- info$name[]
 # The units in the hdf files are in ft, ft^2, ft^3
 
-# Layer 1 --------------
-# indices matrix
-lay1_ids <- GW_BDGinfo[[info$name[7]]][,]
-
-### PUMPING 
-PMP <- array(data = 0, dim = c(32537, 504))
-
-#### Element Pumping ++++++++++++
-# this is the row number for the 	Pumping by Element_Outflow (-) in attribute names
-elem_pmp_att_id <- 20
-# The element i will get its pumping amount from the row ele_pmp_ids[i]
-ele_pmp_ids <- lay1_ids[,elem_pmp_att_id]
-
-tmp_id <- which(ele_pmp_ids != 0)
-# the row number in info for Layer_1/Pumping by Element_Outflow (-)
-ele_pmp_data_id <- 34
-PMP[tmp_id,] <- PMP[tmp_id,] + GW_BDGinfo[[info$name[ele_pmp_data_id]]][,]
-
-#### Well Pumping ++++++++++++
-# this is the row number for the Pumping by Well_Outflow (-) in attribute names
-well_pmp_att_id <- 22
-# The element i will get its pumping amount from the row ele_pmp_ids[i]
-well_pmp_ids <- lay1_ids[,well_pmp_att_id]
-
-tmp_id <- which(well_pmp_ids != 0)
-# the row number in info for 	Layer_1/Pumping by Well_Outflow (-)
-well_pmp_data_id <- 36
-PMP[tmp_id,] <- PMP[tmp_id,] + GW_BDGinfo[[info$name[well_pmp_data_id]]][,]
-
-### STREAM INFLOWS 
-STRM <- array(data = 0, dim = c(32537, 504))
-#### Streams_Inflow (+)  ++++++++++++
-strm_P_att_id <- 3
-strm_P_ids <- lay1_ids[,strm_P_att_id]
-
-tmp_id <- which(strm_P_ids != 0)
-strm_P_ids_data_id <- 44
-STRM[tmp_id,] <- STRM[tmp_id,] + GW_BDGinfo[[info$name[strm_P_ids_data_id]]][,]
-
-#### Streams_Inflow (-)  ++++++++++++
-strm_M_att_id <- 4
-strm_M_ids <- lay1_ids[,strm_M_att_id]
-
-tmp_id <- which(strm_M_ids != 0)
-strm_M_ids_data_id <- 45
-STRM[tmp_id,] <- STRM[tmp_id,] - GW_BDGinfo[[info$name[strm_M_ids_data_id]]][,]
+getDatafromBudget <- function(lay, name){
+  out = vector(mode = "list", length = 2)
+  lay_ids <- GW_BUD[[paste0("Attributes/Layer",lay,"_ElemDataColumns")]][,]
+  elem_att_id <- which(Attribute_names == name)
+  ids <- lay_ids[,elem_att_id]
+  tmp_id <- which(ids != 0)
+  out[[1]] <- tmp_id
+  data_id <- which(info_names == paste0("Layer_", lay, "/", name))
+  out[[2]] <- GW_BUD[[ paste0("Layer_", lay, "/", name) ]][,]
+  return(out)
+}
 
 ### RECHARGE 
 RCH <- array(data = 0, dim = c(32537, 504))
-# Deep percolation
-rch_att_id <- 7 # Deep Percolation_Inflow (+) 
-rch_ids <- lay1_ids[,rch_att_id]
+tmp <- getDatafromBudget(1, "Deep Percolation_Inflow (+)")
+RCH[tmp[[1]],] <- RCH[tmp[[1]],] + tmp[[2]] 
+C2VSIM_RCH <- RCH
+save(list = c("C2VSIM_RCH"), file = "C2VSIM_RCH.RData")
 
-tmp_id <- which(rch_ids != 0)
-rch_ids_data_id <- 26
-RCH[tmp_id,] <- RCH[tmp_id,] + GW_BDGinfo[[info$name[rch_ids_data_id]]][,]
-
-#Small Watershed Percolation_Inflow (+)
-#swp_att_id <- 13 # Small Watershed Percolation_Inflow (+)
-#swp_ids <- lay1_ids[,swp_att_id]
-
-#tmp_id <- which(swp_ids != 0)
-#swp_ids_data_id <- 41
-#RCH[tmp_id,] <- RCH[tmp_id,] + GW_BDGinfo[[info$name[swp_ids_data_id]]][,]
+### STREAMS 
+STRM <- array(data = 0, dim = c(32537, 504))
+tmp <- getDatafromBudget(1, "Streams_Inflow (+)")
+STRM[tmp[[1]],] <- STRM[tmp[[1]],] + tmp[[2]] 
+tmp <- getDatafromBudget(1, "Streams_Outflow (-)")
+STRM[tmp[[1]],] <- STRM[tmp[[1]],] - tmp[[2]] 
 
 
-
-
-
-# Layer 2 --------------
-# indices matrix
-lay2_ids <- GW_BDGinfo[[info$name[8]]][,]
-
-#### Element Pumping ++++++++++++
-# this is the row number for the 	Pumping by Element_Outflow (-) in attribute names
-elem_pmp_att_id <- 20
-# The element i will get its pumping amount from the row ele_pmp_ids[i]
-ele_pmp_ids <- lay2_ids[,elem_pmp_att_id]
-
-tmp_id <- which(ele_pmp_ids != 0)
-# the row number in info for Layer_1/Pumping by Element_Outflow (-)
-ele_pmp_data_id <- 62
-PMP[tmp_id,] <- PMP[tmp_id,] + GW_BDGinfo[[info$name[ele_pmp_data_id]]][,]
-
-#### Well Pumping ++++++++++++
-# this is the row number for the Pumping by Well_Outflow (-) in attribute names
-well_pmp_att_id <- 22
-# The element i will get its pumping amount from the row ele_pmp_ids[i]
-well_pmp_ids <- lay2_ids[,well_pmp_att_id]
-
-tmp_id <- which(well_pmp_ids != 0)
-# the row number in info for 	Layer_1/Pumping by Well_Outflow (-)
-well_pmp_data_id <- 64
-PMP[tmp_id,] <- PMP[tmp_id,] + GW_BDGinfo[[info$name[well_pmp_data_id]]][,]
-
-# Layer 3 --------------
-# indices matrix
-lay3_ids <- GW_BDGinfo[[info$name[9]]][,]
-
-#### Element Pumping ++++++++++++
-# this is the row number for the 	Pumping by Element_Outflow (-) in attribute names
-elem_pmp_att_id <- 20
-# The element i will get its pumping amount from the row ele_pmp_ids[i]
-ele_pmp_ids <- lay3_ids[,elem_pmp_att_id]
-
-tmp_id <- which(ele_pmp_ids != 0)
-# the row number in info for Layer_1/Pumping by Element_Outflow (-)
-ele_pmp_data_id <- 90
-PMP[tmp_id,] <- PMP[tmp_id,] + GW_BDGinfo[[info$name[ele_pmp_data_id]]][,]
-
-#### Well Pumping ++++++++++++
-# this is the row number for the Pumping by Well_Outflow (-) in attribute names
-well_pmp_att_id <- 22
-# The element i will get its pumping amount from the row ele_pmp_ids[i]
-well_pmp_ids <- lay3_ids[,well_pmp_att_id]
-
-tmp_id <- which(well_pmp_ids != 0)
-# the row number in info for 	Layer_1/Pumping by Well_Outflow (-)
-well_pmp_data_id <- 92
-PMP[tmp_id,] <- PMP[tmp_id,] + GW_BDGinfo[[info$name[well_pmp_data_id]]][,]
-
-# Layer 4 --------------
-# indices matrix
-lay4_ids <- GW_BDGinfo[[info$name[10]]][,]
-
-#### Element Pumping ++++++++++++
-# this is the row number for the 	Pumping by Element_Outflow (-) in attribute names
-elem_pmp_att_id <- 20
-# The element i will get its pumping amount from the row ele_pmp_ids[i]
-ele_pmp_ids <- lay4_ids[,elem_pmp_att_id]
-
-tmp_id <- which(ele_pmp_ids != 0)
-# the row number in info for Layer_1/Pumping by Element_Outflow (-)
-ele_pmp_data_id <- 118
-PMP[tmp_id,] <- PMP[tmp_id,] + GW_BDGinfo[[info$name[ele_pmp_data_id]]][,]
-
-#### Well Pumping ++++++++++++
-# this is the row number for the Pumping by Well_Outflow (-) in attribute names
-well_pmp_att_id <- 22
-# The element i will get its pumping amount from the row ele_pmp_ids[i]
-well_pmp_ids <- lay4_ids[,well_pmp_att_id]
-
-tmp_id <- which(well_pmp_ids != 0)
-# the row number in info for 	Layer_1/Pumping by Well_Outflow (-)
-well_pmp_data_id <- 120
-PMP[tmp_id,] <- PMP[tmp_id,] + GW_BDGinfo[[info$name[well_pmp_data_id]]][,]
+### PUMPING 
+PMP <- array(data = 0, dim = c(32537, 504))
+for (i in 1:4) {
+  tmp <- getDatafromBudget(i, "Pumping by Element_Outflow (-)")
+  PMP[tmp[[1]],] <- PMP[tmp[[1]],] + tmp[[2]] 
+  tmp <- getDatafromBudget(i, "Pumping by Well_Outflow (-)")
+  PMP[tmp[[1]],] <- PMP[tmp[[1]],] + tmp[[2]] 
+}
 
 
 
@@ -173,7 +66,7 @@ pmp_yearly <- colSums(array(data = pmp_monthly, dim = c(12,42)))
 strm_yearly <- colSums(array(data = strm_monthly, dim = c(12,42)))
 
 
-
+source("../../../C2VsimCG/Rwrkspc/c2vsim_io.R")
 CVBD <- c2vsim.readGWBUD(filename = paste0(results_path, "C2VSimFG_GW_Budget.bud"),
                          Nsub = 21, Nskip = c(8,rep(9,20)), NtimeSteps = 504,CG = F)
 
@@ -222,7 +115,7 @@ ggplot(plot_df, aes(x = Time),linetype = variable )+
   geom_step(aes(y = strm_FG_BD, color = "Stream leackage (Fine)", linetype="Stream leackage (Fine)"), size = 0.8)+
   #geom_step(aes(y = pmp_FG_my, color = "C2VSIM Fine Pumping V1"), size = 1.2)+
   #geom_step(aes(y = rch_FG_my, color = "C2VSIM Fine Recharge v1"), size = 1.2)+
-  #geom_step(aes(y = strm_FG_my, color = "C2VSIM Fine Stream leackage v1"), size = 1.2)+
+  #geom_step(aes(y = strm_FG_my, color = "C2VSIM Fine Stream leackage v1"), size = 1.2)
   geom_step(aes(y = pmp_CG_BD, color = "Pumping (Coarse)", linetype="Pumping (Coarse)"), size = 1.2) + #, linetype="dotted"
   geom_step(aes(y = rch_CG_BD, color = "Recharge (Coarse)", linetype="Recharge (Coarse)"), size = 1.2)+
   geom_step(aes(y = strm_CG_BD, color = "Stream leackage (Coarse)", linetype="Stream leackage (Coarse)"), size = 1.2)+
@@ -250,3 +143,67 @@ ggplot(plot_df, aes(x = Time),linetype = variable )+
   labs(x="Year", y="MAF")
 
 ggsave(filename = "C2VSIM_Budget_annual.png", plot = g, device="png")  
+
+
+ggplot(plot_df, aes(x = Time))+
+  geom_step(aes(y = strm_FG_my), size = 1.2,color='blue')+
+  geom_step(aes(y = strm_FG_BD), size = 1, color="red")
+
+C2VSIM_CG_BUD <- data.frame(1922:2009,pmp_CG_BD[1:88],rch_CG_BD[1:88],strm_CG_BD[1:88])
+colnames(C2VSIM_CG_BUD) <- c("Time", "Pmp", "Rch", "Strm")
+C2VSIM_FG_BUD <- data.frame(1974:2015, pmp_FG_BD[53:94], rch_FG_BD[53:94], strm_FG_BD[53:94])
+colnames(C2VSIM_FG_BUD) <- c("Time", "Pmp", "Rch", "Strm")
+save(list = c("C2VSIM_CG_BUD","C2VSIM_FG_BUD"), file = "C2VSIMBUD_MAF.RData")
+
+# ------- Caclulate recharge rate per year  -------
+# load the mesh file with the area
+c2vsim_mesh <- readOGR(dsn = "../gis_data/C2Vsim_Elements.shp")
+c2vsim_mesh_polys <- c2vsim_mesh@polygons
+# Calculate the area of Central Valley using the shapefile to make sure that this is in m
+elem_area <- vector(mode = "numeric", length = length(c2vsim_mesh_polys))
+for (i in 1:length(c2vsim_mesh_polys)) {
+  elem_area[i] <- c2vsim_mesh_polys[[i]]@area
+}
+
+RCH <- array(data = 0, dim = c(32537, 504))
+tmp <- getDatafromBudget(1, "Deep Percolation_Inflow (+)")
+RCH[tmp[[1]],] <- RCH[tmp[[1]],] + tmp[[2]]
+
+# Convert Cubic feet to cubic meter
+RCH <- RCH/35.3147
+RCH_YR <- array(data = 0, dim = c(dim(RCH)[1], length(1974:2015)))
+sind <- 1
+for (i in 1:dim(RCH_YR)[2]) {
+  eind <- sind + 11
+  RCH_YR[,i] <- rowSums(RCH[,sind:eind])
+  sind <- sind + 12
+}
+
+
+
+RCH_YR <- 1000*(RCH_YR/elem_area)
+
+non_zero_rch <- which(RCH_YR[,1] != 0)
+tmp <- hist(RCH_YR[non_zero_rch,1],nclass = 200)
+df <- data.frame(tmp$mids,tmp$counts)
+colnames(df) <- c("mids","counts")
+
+p <- plot_ly(data = df)
+for (i in 1:dim(RCH_YR)[2]) {
+  non_zero_rch <- which(RCH_YR[,i] > 3 & RCH_YR[,i] < 1000)
+  tmp <- hist(RCH_YR[non_zero_rch,i],nclass = 75)
+  df <- data.frame(tmp$mids,tmp$counts)
+  colnames(df) <- c("mids","counts")
+  df$counts <- 100*(tmp$counts /sum(tmp$counts))
+  p <- add_trace(p, data = df, x=~mids,y=~counts,
+                 type = 'scatter', mode = 'none', fill = 'tonexty')
+  #p <- add_trace(p, data = df, x=rep(1961+i,length(tmp$mids)),y=~mids,z=~counts,
+  #               type = 'scatter3d', mode = 'lines', fill = 'tonexty')
+}
+
+p1 <- p %>%
+  layout(title = 'C2VSIM FG recharge distributions',
+         xaxis = list(title = "Recharge [mm/year]", titlefont = list(size = 18), zeroline = F),
+         yaxis = list(title = "Percentage of grid cells [%]", titlefont = list(size = 18)), 
+         showlegend = FALSE)
+p1
